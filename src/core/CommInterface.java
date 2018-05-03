595f13beaf96fdcce28f151f08fe3eed7f330d28
Unknown <pierre-elliott.thiboud@etu.univ-lyon1.fr>
2018-05-03 20:10:09 +0200

    Haem...

--- src/core/CommInterface.java
+++ src/core/CommInterface.java
@@ -1,6 +1,22 @@
 package core;
 
-public abstract class Comms {
+import java.net.DatagramSocket;
+import java.net.ServerSocket;
+
+public abstract class CommInterface {
+
+    private Object socket;
+
+    public CommInterface(Object sock) throws Exception {
+        if( !(sock instanceof ServerSocket || sock instanceof DatagramSocket) ) {
+            throw new Exception("Invalid socket provided");
+        }
+        socket = sock;
+    }
+
+    public Object getSocket() {
+        return socket;
+    }
 
     public abstract void init();
 
