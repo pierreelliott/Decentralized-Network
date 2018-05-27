47945ce3b0cee1f56baed072b5bcbd544b7d9611
Unknown <pierre-elliott.thiboud@etu.univ-lyon1.fr>
2018-05-27 22:16:27 +0200

    Ménage dans les classes

--- src/core/Utils.java
+++ src/core/Utils.java
@@ -2,6 +2,8 @@ package core;
 
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.util.ArrayList;
+import java.util.List;
 
 public abstract class Utils extends CommInterface implements Runnable {
 
@@ -29,5 +31,39 @@ public abstract class Utils extends CommInterface implements Runnable {
         } catch(Exception e) {
             e.printStackTrace();
         }
+    }
+
+    /* ================== STATIC ======================== */
+
+    public static List<Integer> scan(int dep, int fin) {
+        List<Integer> list = new ArrayList<>();
+        DatagramSocket ds = null;
+        for(int i = dep; i <= fin; i++) {
+            try {
+                ds = new DatagramSocket(i);
+            } catch (Exception ex) {
+                list.add(i);
+            } finally {
+                if(ds != null) {
+                    ds.close();
+                }
+                ds = null;
+            }
+        }
+        return list;
+    }
+
+    public static boolean isOccuped(int port) {
+        DatagramSocket ds = null;
+        try {
+            ds = new DatagramSocket(port);
+        } catch (Exception ex) {
+            return false;
+        } finally {
+            if(ds != null) {
+                ds.close();
+            }
+        }
+        return true;
     }
 }
